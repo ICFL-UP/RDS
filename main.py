c3f97785ccf81b30d597c1bab9fdcceec2c9dbc5
AvinashSingh786 <tashan.avi@gmail.com>
2022-12-23 18:58:07 +0200

    balanced dataset

--- main.py
+++ main.py
@@ -17,13 +17,24 @@ import traceback
 
 def main():
     TRAIN = True
-    DATA = False
+    DATA = True
     PREDICT = False
 
-    data_filename = "14_Ransomware_Detection_Using_Strings.csv"
-    prefix = "14_"
+    data_filename = "B14_Ransomware_Detection_Using_Strings.csv"
+    prefix = "B14_"
 
     print(datetime.now())
+    # df = pd.read_csv(data_filename)
+    # df = df.groupby('label')
+    # df = df.apply(lambda x: x.sample(df.size().min()).reset_index(drop=True))
+    # df.to_csv("B"+data_filename)
+    
+    # # STATS 
+    data = pd.read_csv(data_filename)
+    log.log("Dataset stats - Category: " + str(data["category"].value_counts()))
+    log.log("Dataset stats - Label: " + str(data["label"].value_counts()))
+    log.log("Dataset stats - shape: " + str(data.shape))
+   
 
     if DATA:
         log.log("Preparing data splitting ...")
@@ -66,42 +77,38 @@ def main():
         }
     }
 
-    # data = pd.read_csv(data_filename)
-    # log.log("Dataset stats - Category: " + str(data["category"].value_counts()))
-    # log.log("Dataset stats - Label: " + str(data["label"].value_counts()))
-    # log.log("Dataset stats - shape: " + str(data.shape))
+ 
+    log.log("Train BOW" + str(data_reader.stats(X["TRAIN"]["BOW"], Y["TRAIN"]["BOW"])))
+    log.log("Train DOC2VEC" + str(data_reader.stats(np.array(X["TRAIN"]["DOC2VEC"]), Y["TRAIN"]["DOC2VEC"])))
+    log.log("Train TFIDF" + str(data_reader.stats(X["TRAIN"]["TFIDF"], Y["TRAIN"]["TFIDF"])))
     
-    # log.log("Train BOW" + str(data_reader.stats(X["TRAIN"]["BOW"], Y["TRAIN"]["BOW"])))
-    # log.log("Train DOC2VEC" + str(data_reader.stats(np.array(X["TRAIN"]["DOC2VEC"]), Y["TRAIN"]["DOC2VEC"])))
-    # log.log("Train TFIDF" + str(data_reader.stats(X["TRAIN"]["TFIDF"], Y["TRAIN"]["TFIDF"])))
+    log.log("VAL BOW" + str(data_reader.stats(X["VAL"]["BOW"], Y["VAL"]["BOW"])))
+    log.log("VAL DOC2VEC" + str(data_reader.stats(np.array(X["VAL"]["DOC2VEC"]), Y["VAL"]["DOC2VEC"])))
+    log.log("VAL TFIDF" + str(data_reader.stats(X["VAL"]["TFIDF"], Y["VAL"]["TFIDF"])))
     
-    # log.log("VAL BOW" + str(data_reader.stats(X["VAL"]["BOW"], Y["VAL"]["BOW"])))
-    # log.log("VAL DOC2VEC" + str(data_reader.stats(np.array(X["VAL"]["DOC2VEC"]), Y["VAL"]["DOC2VEC"])))
-    # log.log("VAL TFIDF" + str(data_reader.stats(X["VAL"]["TFIDF"], Y["VAL"]["TFIDF"])))
-    
-    # log.log("TEST BOW" + str(data_reader.stats(X["TEST"]["BOW"], Y["TEST"]["BOW"])))
-    # log.log("TEST DOC2VEC" + str(data_reader.stats(np.array(X["TEST"]["DOC2VEC"]), Y["TEST"]["DOC2VEC"])))
-    # log.log("TEST TFIDF" + str(data_reader.stats(X["TEST"]["TFIDF"], Y["TEST"]["TFIDF"])))
+    log.log("TEST BOW" + str(data_reader.stats(X["TEST"]["BOW"], Y["TEST"]["BOW"])))
+    log.log("TEST DOC2VEC" + str(data_reader.stats(np.array(X["TEST"]["DOC2VEC"]), Y["TEST"]["DOC2VEC"])))
+    log.log("TEST TFIDF" + str(data_reader.stats(X["TEST"]["TFIDF"], Y["TEST"]["TFIDF"])))
  
     if TRAIN:
         # Classifier Training
         log.log("\n\nTraining Classifiers ...")
-        # try:
-        #     classifiers.randomForrest(X["TRAIN"]["BOW"], Y["TRAIN"]["BOW"], "BOW")
-        #     classifiers.randomForrest(X["TRAIN"]["DOC2VEC"], Y["TRAIN"]["DOC2VEC"], "Doc2Vec")
-        #     classifiers.randomForrest(X["TRAIN"]["TFIDF"], Y["TRAIN"]["TFIDF"], "TFIDF")
-        # except:
-        #     print(traceback.print_exc())
-        #     log.log("\n\n\n\n\nERROR in training of RF\n\n\n\n")
+        try:
+            classifiers.randomForrest(X["TRAIN"]["BOW"], Y["TRAIN"]["BOW"], "BOW")
+            classifiers.randomForrest(X["TRAIN"]["DOC2VEC"], Y["TRAIN"]["DOC2VEC"], "Doc2Vec")
+            classifiers.randomForrest(X["TRAIN"]["TFIDF"], Y["TRAIN"]["TFIDF"], "TFIDF")
+        except:
+            print(traceback.print_exc())
+            log.log("\n\n\n\n\nERROR in training of RF\n\n\n\n")
         
 
-        # try:
-        #     classifiers.adaBoost(X["TRAIN"]["BOW"], Y["TRAIN"]["BOW"], "BOW")
-        #     classifiers.adaBoost(X["TRAIN"]["DOC2VEC"], Y["TRAIN"]["DOC2VEC"], "Doc2Vec")
-        #     classifiers.adaBoost(X["TRAIN"]["TFIDF"], Y["TRAIN"]["TFIDF"], "TFIDF")
-        # except:
-        #     print(traceback.print_exc())
-        #     log.log("\n\n\n\n\nERROR in training of AB\n\n\n\n")
+        try:
+            classifiers.adaBoost(X["TRAIN"]["BOW"], Y["TRAIN"]["BOW"], "BOW")
+            classifiers.adaBoost(X["TRAIN"]["DOC2VEC"], Y["TRAIN"]["DOC2VEC"], "Doc2Vec")
+            classifiers.adaBoost(X["TRAIN"]["TFIDF"], Y["TRAIN"]["TFIDF"], "TFIDF")
+        except:
+            print(traceback.print_exc())
+            log.log("\n\n\n\n\nERROR in training of AB\n\n\n\n")
             
 
         try:
@@ -113,22 +120,22 @@ def main():
             log.log("\n\n\n\n\nERROR in training of SVM\n\n\n\n")
         
 
-        # try:
-        #     classifiers.knn(X["TRAIN"]["BOW"], Y["TRAIN"]["BOW"], "BOW")
-        #     classifiers.knn(X["TRAIN"]["DOC2VEC"], Y["TRAIN"]["DOC2VEC"], "Doc2Vec")
-        #     classifiers.knn(X["TRAIN"]["TFIDF"], Y["TRAIN"]["TFIDF"], "TFIDF")
-        # except:
-        #     print(traceback.print_exc())
-        #     log.log("\n\n\n\n\nERROR in training of KNN\n\n\n\n")
+        try:
+            classifiers.knn(X["TRAIN"]["BOW"], Y["TRAIN"]["BOW"], "BOW")
+            classifiers.knn(X["TRAIN"]["DOC2VEC"], Y["TRAIN"]["DOC2VEC"], "Doc2Vec")
+            classifiers.knn(X["TRAIN"]["TFIDF"], Y["TRAIN"]["TFIDF"], "TFIDF")
+        except:
+            print(traceback.print_exc())
+            log.log("\n\n\n\n\nERROR in training of KNN\n\n\n\n")
         
 
-        # try:
-        #     classifiers.decisionTree(X["TRAIN"]["BOW"], Y["TRAIN"]["BOW"], "BOW")
-        #     classifiers.decisionTree(X["TRAIN"]["DOC2VEC"], Y["TRAIN"]["DOC2VEC"], "Doc2Vec")
-        #     classifiers.decisionTree(X["TRAIN"]["TFIDF"], Y["TRAIN"]["TFIDF"], "TFIDF")
-        # except:
-        #     print(traceback.print_exc())   
-        #     log.log("\n\n\n\n\nERROR in training of DT\n\n\n\n")
+        try:
+            classifiers.decisionTree(X["TRAIN"]["BOW"], Y["TRAIN"]["BOW"], "BOW")
+            classifiers.decisionTree(X["TRAIN"]["DOC2VEC"], Y["TRAIN"]["DOC2VEC"], "Doc2Vec")
+            classifiers.decisionTree(X["TRAIN"]["TFIDF"], Y["TRAIN"]["TFIDF"], "TFIDF")
+        except:
+            print(traceback.print_exc())   
+            log.log("\n\n\n\n\nERROR in training of DT\n\n\n\n")
         
 
     
@@ -137,139 +144,24 @@ def main():
         # Predict
         log.log("\n\nPREDICTING ...\n\n")
         models = {}
-        for mdl in ['KNN']:#, 'DT', 'RF', 'AB', 'SVM', 'KNN']:
+        for mdl in ['DT', 'RF', 'AB', 'SVM', 'KNN']:
             for nlp in ['BOW', 'TFIDF', 'DOC2VEC']:
                 models[mdl+"_"+nlp] = joblib.load('Models/{}_{}_model.pkl'.format(mdl, nlp))
-                classifiers.evaluate_model(mdl+"_"+nlp, models[mdl+"_"+nlp], X["VAL"][nlp], Y["VAL"][nlp])
+                # classifiers.evaluate_model(mdl+"_"+nlp, models[mdl+"_"+nlp], X["VAL"][nlp], Y["VAL"][nlp])
 
+        ##ROC Curve
+        # for nlp in ['BOW', 'TFIDF', 'DOC2VEC']:
+        #     fig = plt.figure(figsize=(7, 7), dpi=300)
+        #     axes = fig.gca()
+        #     for x in  ['DT', 'RF', 'AB', 'SVM', 'KNN']:
+        #         RocCurveDisplay.from_estimator(models[x+"_"+nlp], X["VAL"][nlp], Y["VAL"][nlp], ax=axes)
+        #     # plt.show()
+        #     plt.safe(nlp+".png")
 
-
-
-
-
-
-
-
-
-    # # data_reader.createStats()
-    # overall_classes = []
-    # overall_predictions = []
-    # predicted_proba = [[], [], [], [], []]
-    # accuracies = [0.0, 0.0, 100.0]
-    # f1_scores = [0.0, 0.0, 100.0]
-    # precision_scores = [0.0, 0.0, 100.0]
-    # tf_predictions = [0, 0, 0, 0]  # True Positive, True Negative, False Positive, False Negative
-    # num_runs = 1
-
-    # estimators = []
-    # shuffled_test_arr = [[], [], [], [], []]
-    # shuffled_classes_test_arr = [[], [], [], [], []]
-    # # for c_index in range(5):
-    # for i in range(num_runs):
-    #     start_time = time.time()
-    #     # Average runs
-    #     # data = data_reader.getTrainTest(0.2)
-    #     # K-Fold
-    #     kf = KFold(n_splits=5)
-    #     data = data_reader.getTrainTestFromCSV(-1, "12_Ransomware_Detection_using_Strings.csv")
-
-    #     for f, (train_index, test_index) in enumerate(kf.split(data[0])):
-
-    #         split_data = ([data[0][x] for x in train_index], [data[1][x] for x in train_index]), \
-    #                 ([data[0][x] for x in test_index], [data[1][x] for x in test_index])
-
-    #         # Bag-of-words
-    #         # vector = preprocessor.bagOfWords(split_data[0][0] + split_data[1][0])[0].toarray()
-    #         # TF-IDF
-    #         vector = preprocessor.TF_IDF(split_data[0][0] + split_data[1][0])[0].toarray()
-    #         # Doc2Vec
-    #         # model = preprocessor.doc2Vec([TaggedDocument(doc, [i]) for i, doc in enumerate(split_data[0][0] + split_data[1][0])])
-    #         # vector = [model.infer_vector(x.split()) for x in split_data[0][0] + split_data[1][0]]
-
-    #         train_vector = vector[:len(split_data[0][0])]
-    #         test_vector = vector[len(split_data[0][0]):]
-    #         shuffled_train, shuffled_classes = shuffle(train_vector, split_data[0][1])
-    #         # if c_index == 0:
-    #         #     # Random Forrest
-    #         # classifier = classifiers.randomForrest(shuffled_train, shuffled_classes)
-    #         # elif c_index == 1:
-    #         #     # AdaBoost
-    #         # classifier = classifiers.adaBoost(shuffled_train, shuffled_classes)
-    #         # elif c_index == 2:
-    #         #     # SVM
-    #         # classifier = classifiers.svm(shuffled_train, shuffled_classes)
-    #         # elif c_index == 3:
-    #         #     # KNN
-    #         classifier = classifiers.knn(shuffled_train, shuffled_classes)
-    #         # elif c_index == 4:
-    #         #     # Decision Tree
-    #         #     classifier = classifiers.decisionTree(shuffled_train, shuffled_classes)
-
-    #         estimators.append(classifier)
-    #         shuffled_test, shuffled_classes_test = shuffle(test_vector, split_data[1][1])
-    #         # shuffled_test_arr[c_index] += shuffled_test.tolist()
-    #         # shuffled_classes_test_arr[c_index] += shuffled_classes_test
-    #         correct_predictions = 0
-    #         predicted_values = []
-
-    #         for x in range(len(shuffled_classes_test)):
-    #             predicted_value = classifier.predict([shuffled_test[x]])[0]
-    #             # predicted_proba[c_index].append(classifier.predict_proba([shuffled_test[x]]))
-    #             predicted_values.append(predicted_value)
-    #             if predicted_value == shuffled_classes_test[x]:
-    #                 correct_predictions += 1
-    #                 if predicted_value == 0:
-    #                     tf_predictions[1] += 1
-    #                 else:
-    #                     tf_predictions[0] += 1
-    #             else:
-    #                 if predicted_value == 0:
-    #                     tf_predictions[3] += 1
-    #                 else:
-    #                     tf_predictions[2] += 1
-
-    #         accuracy = (correct_predictions / len(test_vector)) * 100
-    #         f1 = f1_score(shuffled_classes_test, predicted_values) * 100
-    #         precision = precision_score(shuffled_classes_test, predicted_values) * 100
-    #         accuracies[0] += accuracy / num_runs
-    #         # accuracies_valid[0] += accuracy_valid/num_runs
-    #         f1_scores[0] += f1 / num_runs
-    #         # f1_scores_valid[0] += f1_valid/num_runs
-    #         precision_scores[0] += precision / num_runs
-    #         # precision_scores_valid[0] += precision_valid/num_runs
-    #         if accuracies[1] < accuracy:
-    #             accuracies[1] = accuracy
-    #         if f1_scores[1] < f1:
-    #             f1_scores[1] = f1
-    #         if precision_scores[1] < precision:
-    #             precision_scores[1] = precision
-
-    #         if accuracies[2] > accuracy:
-    #             accuracies[2] = accuracy
-    #         if f1_scores[2] > f1:
-    #             f1_scores[2] = f1
-    #         if precision_scores[2] > precision:
-    #             precision_scores[2] = precision
-    #         overall_classes += shuffled_classes_test
-    #         overall_predictions += predicted_values
-    #         print("\n => TIME TAKEN: " + str((time.time() - start_time) / 60))
-
-    # # fig = plt.figure(figsize=(7, 7), dpi=300)
-    # # axes = fig.gca()
-    # # for x in range(5):
-    # #     RocCurveDisplay.from_estimator(estimators[x], shuffled_test_arr[x], shuffled_classes_test_arr[x], ax=axes)
-    # # plt.show()
-
-    # print(classification_report(overall_classes, overall_predictions, target_names=["Benign", "Malicious"], digits=4))
-    # print("Average Accuracy: " + str(accuracies[0]) + "\nBest: " + str(accuracies[1]) + "\nWorst: "
-    #       + str(accuracies[2]) + '\n')
-    # print("Average F1: " + str(f1_scores[0]) + "\nBest: " + str(f1_scores[1]) + "\nWorst: " + str(
-    #     f1_scores[2]) + '\n')
-    # print("Average Precision: " + str(precision_scores[0]) + "\nBest: " + str(precision_scores[1]) + "\nWorst: "
-    #       + str(precision_scores[2]))
-    # print("True Positive: " + str(tf_predictions[0]) + " True Negative: " + str(tf_predictions[1])
-    #       + " False positive: " + str(tf_predictions[2]) + " False negative: " + str(tf_predictions[3]) + '\n\n')
-
+        # # BEST MODELS
+        # classifiers.evaluate_model("AB_TFIDF", joblib.load('Models/AB_TFIDF_model.pkl'), X["TEST"]["TFIDF"], Y["TEST"]["TFIDF"])
+        # classifiers.evaluate_model("SVM_BOW", joblib.load('Models/SVM_BOW_model.pkl'), X["TEST"]["BOW"], Y["TEST"]["BOW"])
+        # classifiers.evaluate_model("DT_TFIDF", joblib.load('Models/DT_TFIDF_model.pkl'), X["TEST"]["TFIDF"], Y["TEST"]["TFIDF"])
 
 if __name__ == "__main__":
     main()
